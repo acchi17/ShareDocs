c500e515c89ac8bad4e685000d67585f6d74c60c
acchi17 <grf317@gmail.com>
2021-04-11 13:03:21 +0900

    change

--- StudyC/SampleFile/Chapter4/powerOf2.c
+++ StudyC/SampleFile/Chapter4/powerOf2.c
@@ -4,8 +4,11 @@ int main(void)
 {
    int base, exponent, result, i;
 
+   printf("Input integral value >>");
+   scanf("%d", &exponent);
+
    base = 2;
-   exponent = 10;
+   //exponent = 50;
    result = 1;
 
    for(i=0; i<exponent; i++)
